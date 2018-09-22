1d7198539327917f4b14f763a6765c53356c52a7
andersonstorres <andersonstorres@gmail.com>
2018-09-22 06:46:10 -0300

    Primeiro Commit

---
+++ api/Diligitis/src/test/java/com/diligitis/api/Diligitis/DiligitisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diligitis.api.Diligitis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiligitisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
